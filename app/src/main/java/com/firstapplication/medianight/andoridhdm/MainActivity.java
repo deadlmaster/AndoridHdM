94ac8e5b329239df9ea93a098afa8a782ed921df
Peter Tan <dead-master@msn.com>
2014-12-17 00:33:22 +0100

    added screens

--- app/src/main/java/com/firstapplication/medianight/andoridhdm/MainActivity.java
+++ app/src/main/java/com/firstapplication/medianight/andoridhdm/MainActivity.java
@@ -6,12 +6,14 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
+
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
 
@@ -36,4 +38,6 @@ public class MainActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
