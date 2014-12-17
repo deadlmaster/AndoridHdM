62fe0c390aa332a0545464fad7f80fa9736975e4
Peter Tan <dead-master@msn.com>
2014-12-17 13:49:41 +0100

    code change in popupwindow

--- app/src/main/java/com/firstapplication/medianight/andoridhdm/MainActivity.java
+++ app/src/main/java/com/firstapplication/medianight/andoridhdm/MainActivity.java
@@ -2,22 +2,29 @@ package com.firstapplication.medianight.andoridhdm;
 
 import android.app.Activity;
 import android.content.ClipData;
+//import android.content.Context;
 import android.os.Bundle;
 import android.view.DragEvent;
+//import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
+//import android.widget.PopupWindow;
 
 
 public class MainActivity extends Activity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ImageView coinbutton, main_saving, main_expenditure, main_income, main_credit, main_debts;
+
 
         // dragable item
 
@@ -61,6 +68,7 @@ public class MainActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+
     private final class ChoiceTouchListener implements View.OnTouchListener {
 
         @Override
@@ -93,14 +101,29 @@ public class MainActivity extends Activity {
                 case DragEvent.ACTION_DRAG_EXITED:
                     break;
                 case DragEvent.ACTION_DROP:
+
                     break;
                 case DragEvent.ACTION_DRAG_ENDED:
                     break;
+
                 default:
                     break;
             }
         return true;
         }
     }
+
+/*
+
+    private void initiatePopupWindow() {
+
+            LayoutInflater inflater = (LayoutInflater) MainActivity.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View popuplayout = inflater.inflate(R.layout.popupwindow_layout, null);
+            PopupWindow popupWindow = new PopupWindow(popuplayout, 350, 400, true);
+
+
+    }
+
+*/
 
 }
