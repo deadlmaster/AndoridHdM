bdea2a33868216f41fa14ded3e4b823338bc069f
Peter Tan <dead-master@msn.com>
2014-12-17 09:52:10 +0100

    Drag and Drop implemented Activity_Main changed height and width of pictures

--- app/src/main/java/com/firstapplication/medianight/andoridhdm/MainActivity.java
+++ app/src/main/java/com/firstapplication/medianight/andoridhdm/MainActivity.java
@@ -1,10 +1,14 @@
 package com.firstapplication.medianight.andoridhdm;
 
 import android.app.Activity;
+import android.content.ClipData;
 import android.os.Bundle;
+import android.view.DragEvent;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
 
 
 public class MainActivity extends Activity {
@@ -13,7 +17,25 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView coinbutton, main_saving, main_expenditure, main_income, main_credit, main_debts;
 
+        // dragable item
+
+        coinbutton = (ImageView)findViewById(R.id.Coin_Main);
+        coinbutton.setOnTouchListener(new ChoiceTouchListener());
+
+        // dropable options
+        main_saving = (ImageView)findViewById(R.id.main_saving);
+        main_expenditure = (ImageView)findViewById(R.id.main_expenditure);
+        main_income = (ImageView)findViewById(R.id.main_income);
+        main_credit = (ImageView)findViewById(R.id.main_credit);
+        main_debts = (ImageView)findViewById(R.id.main_debts);
+
+        main_saving.setOnDragListener(new ChoiceDragListener());
+        main_expenditure.setOnDragListener(new ChoiceDragListener());
+        main_income.setOnDragListener(new ChoiceDragListener());
+        main_credit.setOnDragListener(new ChoiceDragListener());
+        main_debts.setOnDragListener(new ChoiceDragListener());
     }
 
 
@@ -39,5 +61,46 @@ public class MainActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+    private final class ChoiceTouchListener implements View.OnTouchListener {
+
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+
+            if (event.getAction() == MotionEvent.ACTION_DOWN) {
+
+                ClipData clipdata = ClipData.newPlainText("","");
+                View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(v);
+                v.startDrag(clipdata,shadowBuilder,v,0);
+
+                return true;
+
+        } else {
+                    return  false;
+            }
+        }
+    }
+
+    private class ChoiceDragListener implements View.OnDragListener{
+
+        @Override
+        public boolean onDrag(View v, DragEvent dragevent) {
+
+            switch (dragevent.getAction()) {
+                case DragEvent.ACTION_DRAG_STARTED:
+                    break;
+                case DragEvent.ACTION_DRAG_ENTERED:
+                    break;
+                case DragEvent.ACTION_DRAG_EXITED:
+                    break;
+                case DragEvent.ACTION_DROP:
+                    break;
+                case DragEvent.ACTION_DRAG_ENDED:
+                    break;
+                default:
+                    break;
+            }
+        return true;
+        }
+    }
 
 }
